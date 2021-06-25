018f78c1659050d0129ea67ea3ce6687df70339b
WeiWu2 <weidiwu207@gmail.com>
2021-06-25 17:09:47 -0400

    changes header image and adds profile image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import './App.css';
-import Nav from './Nav.js';
-import profile from './assets/profileIMG2.png';
+import "./App.css";
+import Nav from "./Nav.js";
+import profileImage from "./assets/profileIMG.png";
 function App() {
   return (
     <div className="home-container">
-      <Nav/>
-      <div className='home-img-container'>
-        <div className='profile-img'> </div>
-      {/* <img className='profile-img' src= {profile} alt='Wei Wu'/> */}
+      <Nav />
+      <div className="home-img">
+        <h1 className="home-header-text">
+          FULL STACK <h1>WEB</h1>
+        </h1>
       </div>
-    
+      {/* <div className='profile-img'> 
+      <h2></h2>
+      </div> */}
+      <div className="about-me">
+        <div>
+          <img src={profileImage} alt="Wei Wu" />
+        </div>
+        <div>
+          <h2>About me</h2>
+          <p>Hello, I'm Wei</p>
+        </div>
+      </div>
+      <div className='skills'>
+        <h2>Skills</h2>
+      </div>
     </div>
   );
 }
