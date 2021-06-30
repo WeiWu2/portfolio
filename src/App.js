d9b33e89b4a14087c373adb7026319e2b59261a8
WeiWu2 <weidiwu207@gmail.com>
2021-06-29 21:08:25 -0400

    adds skills section and svgs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import Nav from "./Nav.js";
 import profileImage from "./assets/profileIMG.png";
+import reactLogo from "./assets/react-logo.svg";
+import jsLogo from "./assets/javascript-logo.svg";
+import cssLogo from "./assets/css-logo.svg";
+import htmlLogo from "./assets/html-logo.svg";
 function App() {
   return (
     <div className="home-container">
@@ -22,8 +26,45 @@ function App() {
           <p>Hello, I'm Wei</p>
         </div>
       </div>
-      <div className='skills'>
+      <div className='skills-container'>
         <h2>Skills</h2>
+        <div className='skills-list'>
+          <div className='skills'>
+          <img src={reactLogo} alt='React.js'></img>
+          <p>React</p>
+          </div>
+          <div className='skills'>
+          <img src={jsLogo} alt='Javascript'></img>
+          <p>Javascript</p>
+          </div>
+          <div className='skills'>
+          <img src={htmlLogo} alt='HTML5'></img>
+          <p>HTML5</p>
+          </div>
+          <div className='skills'>
+          <img src={cssLogo} alt='CSS3'></img>
+          <p>CSS3</p>
+          </div>
+          <div className='skills'>
+          <img src={cssLogo} alt='CSS3'></img>
+          <p>Node.js</p>
+          </div>
+          <div className='skills'>
+          <img src={cssLogo} alt='CSS3'></img>
+          <p>Git</p>
+          </div>
+          <div className='skills'>
+          <img src={cssLogo} alt='CSS3'></img>
+          <p>Redux</p>
+          </div>
+          <div className='skills'>
+          <img src={cssLogo} alt='CSS3'></img>
+          <p>Postgres</p>
+          </div>
+        </div>
+      </div>
+      <div className='projects-container'>
+
       </div>
     </div>
   );
